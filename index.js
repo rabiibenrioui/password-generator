d3112fb7670bf3482c8aaa6eb5417962f44e3697
Rabii <rabii.myself@gmail.com>
2025-12-14 23:52:20 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function GeneratePassword(allowLowercase, allowUppercase, allowNumbers, allowSymbols)
+{
+    
+    return '';
+}
+
+const allowLowercase = true;
+const allowUppercase = true;
+const allowNumbers = true;
+const allowSymbols = true;
+
+const password = GeneratePassword(allowLowercase, allowUppercase, allowNumbers, allowSymbols);
+
+console.log("The generated password is: " + password);
