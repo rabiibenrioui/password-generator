e1aed8a77669bfa908f834ae9ce2d93ef8091a93
Rabii <rabii.myself@gmail.com>
2025-12-15 00:25:27 +0100

    finished the prototype of the project

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-function GeneratePassword(allowLowercase, allowUppercase, allowNumbers, allowSymbols)
+function GeneratePassword(passwordLength, allowLowercase, allowUppercase, allowNumbers, allowSymbols)
 {
-    
-    return '';
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789"
+    const symbolChars = "!@#$&*"
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += allowLowercase ? lowercaseChars : "";
+    allowedChars += allowUppercase ? uppercaseChars : "";
+    allowedChars += allowNumbers ? numberChars : "";
+    allowedChars += allowSymbols ? symbolChars : "";
+
+    for(let i = 0; i < passwordLength; i++)
+    {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length)
+        password += allowedChars[randomIndex];
+    }
+
+    if(password <= 0)
+    {
+        password = "(The password should be at least 1 character long)"
+    } 
+    else if(allowedChars.length === 0)
+    {
+        password = "(The password should contain at least 1 characteristic)"
+    }
+
+    return password;
 }
 
+const passwordLength = 10;
 const allowLowercase = true;
 const allowUppercase = true;
 const allowNumbers = true;
 const allowSymbols = true;
 
-const password = GeneratePassword(allowLowercase, allowUppercase, allowNumbers, allowSymbols);
-
+const password = GeneratePassword(passwordLength, allowLowercase, allowUppercase, allowNumbers, allowSymbols)
 console.log("The generated password is: " + password);
